2f48559f11a3c5283420f11bc57932a15c2802ee
williamxmejia <willx1995@gmail.com>
2022-12-29 15:21:24 -0600

    completed setup, almost done with the front end

--- app.mjs
+++ app.mjs
@@ -1,29 +1,38 @@
+import express from "express";
+import mysql2 from "mysql2";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
 
-import express from "express"
-import mysql2 from "mysql2"
-import cors from "cors"
-import dotenv from "dotenv"
+const app = express();
 
-const app = express()
+const __dirname = path.resolve();
 
-dotenv.config()
-console.log(process.env)
+dotenv.config();
+
+console.log(__dirname);
+// console.log(process.cwd)
+// console.log(__dirname)
 
 const connection = mysql2.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: 'testing'
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: "testing",
 });
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+app.use("/static", express.static(__dirname + "/public"));
 
-connection.query("SELECT * FROM users", (err, results) => {
-    if(err) throw err;
-    console.log(results)
-})
+// connection.query("SELECT * FROM users", (err, results) => {
+//     if(err) throw err;
+//     console.log(results)
+// })
 
+app.get("/", (req, res) => {
+  res.render("pages/index");
+});
 
 app.listen(3000, () => {
-    console.log("Connected")
-})
+  console.log("Connected");
+});
