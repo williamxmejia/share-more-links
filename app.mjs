3113b2ffd7f72f5449339cf455439a412542394b
williamxmejia <willx1995@gmail.com>
2022-12-29 23:39:24 -0600

    was able to do the redirects and the the post requests

--- app.mjs
+++ app.mjs
@@ -19,28 +19,54 @@ const connection = mysql2.createConnection({
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
-  database: "testing",
+  database: process.env.DATABASE,
 });
 
 app.set("view engine", "ejs");
 app.use("/static", express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({
-    extended:true
-}))
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-// connection.query("SELECT * FROM users", (err, results) => {
-//     if(err) throw err;
-//     console.log(results)
-// })
+connection.query("SELECT website FROM url WHERE shortened_url= 'tulxm'", (err, results) => {
+    if(err) throw err;
+    console.log(results[0].website)
+})
 
 app.get("/", (req, res) => {
-    // console.log(req.body)
-  res.render("pages/index");
+  // console.log(req.body)
+  res.render("pages/index")
 });
 
 app.post("/", (req, res) => {
-    console.log(req.body)
+  let endPoint = (Math.random() + 1).toString(36).substring(7);
+  let website = req.body.input;
+  let sql = "INSERT INTO url(website, shortened_url) VALUES(?, ?)";
 
+  connection.query(sql, [website, endPoint], (err, result) => {
+    if (err) throw err
+  });
+
+  res.send(endPoint)
+});
+
+app.get("/:endpoint", (req,res) => {
+    let sql = `SELECT WEBSITE FROM url WHERE shortened_url=${mysql2.escape(req.params.endpoint)}` 
+    let website = "";
+    // console.log(req.params.endpoint)
+
+    connection.query(sql, (err, result) => {
+        if(err) throw err
+
+        website =result[0].WEBSITE
+        console.log(website)
+
+        res.redirect(301, `https://${website}`)
+
+    })
+    
 })
 
 app.listen(3000, () => {
--- database/url.sql
+++ database/url.sql
@@ -2,7 +2,7 @@ USE shortened_db;
 
 CREATE table url (
     id INT AUTO_INCREMENT,
-    website TINYTEXT,
+    website TEXT,
     shortened_url VARCHAR(255) UNIQUE,
     PRIMARY KEY(id)
 );
